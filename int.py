1adf9e607a17c516b95a88f3e7d7d4eeb94908e0
Jemi patel <120568492+jemee2211@users.noreply.github.com>
2023-04-29 13:29:22 +0200

    Add files via upload

--- int.py
+++ int.py
@@ -2,10 +2,18 @@
 
 first_name = "jemee"
 last_name = "dedaniya"
-age = "21"
+age = 21
 percentage = "78.89"
 name = True
 print(first_name , last_name)
-print("Age = ",str(age))
-print("percentage = ",str(percentage))
+print("Age = ",age)
+print("percentage = ",percentage)
 print("jemee and Vasu are siblings",name)
+print(len(first_name))
+print(last_name.find("d"))
+print(last_name.capitalize())
+print(last_name.upper())
+print(first_name.lower())
+print(first_name.replace("m","n"))
+print(age*2)
+print(first_name*3)
