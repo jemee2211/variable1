763670a87422ac2f5a41e3090ba20d3bba2cb24a
Jemi patel <120568492+jemee2211@users.noreply.github.com>
2023-04-29 12:27:17 +0200

    Add files via upload

---
+++ int.py
@@ -0,0 +1,11 @@
+#variable in python
+
+first_name = "jemee"
+last_name = "dedaniya"
+age = "21"
+percentage = "78.89"
+name = True
+print(first_name , last_name)
+print("Age = ",str(age))
+print("percentage = ",str(percentage))
+print("jemee and Vasu are siblings",name)
